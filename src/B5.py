cfbed166c5acadae65c590cba00593f0aede378e
TilNeubauer <nt1703@mci4me.at>
2025-10-14 17:38:30 +0200

    feat: ValueError N must be >0 for Exercise B.6

--- src/B5.py
+++ src/B5.py
@@ -1,8 +1,11 @@
 import random
 import math
 
-def in_unit_circle(N):
+def in_unit_circle(N: int) ->int:
     # counts how many random N are under the unitcircle  
+
+    if N <= 0:
+        raise ValueError("N must be >0")
     M = 0
 
     for _ in range(N):
@@ -14,7 +17,7 @@ def in_unit_circle(N):
 
     return M
 
-def estimate_pi(N):
+def estimate_pi(N: int) -> float:
     # estimats pi with the formular 
     #   pi = 4 * M/N
     #       N: random points 
@@ -22,15 +25,15 @@ def estimate_pi(N):
 
     M = in_unit_circle(N)
 
-    return 4*M/N  
+    return 4*M/N
 
 
-def get_accuracy(N):
+def get_accuracy(N: int) -> float:
 
     est_pi = estimate_pi(N)
 
     return abs(est_pi - math.pi)
 
 
-print(f"delta pi{get_accuracy(1000)}")
-print(f"pi {estimate_pi(1000)}")
+print(f"delta pi = {get_accuracy(1000)}")
+print(f"pi = {estimate_pi(1000)}")
