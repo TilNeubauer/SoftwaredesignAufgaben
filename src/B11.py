5c79087f1525e60a34d9c8cdd70b08a30525b7d8
TilNeubauer <nt1703@mci4me.at>
2025-10-15 11:11:41 +0200

    feat: new File <B11.py> for Exercise B.11

---
+++ src/B11.py
@@ -0,0 +1,10 @@
+
+
+def main() -> None:
+    lst = ["fist", "second", "third"]
+
+    for index, value in enumerate(lst):
+        print(f"index = {index}, value = '{value}'")
+
+if __name__ == "__main__":
+    main()
