26df0fe37dc1d134db7fd0a23c424ab4927a5d89
TilNeubauer <nt1703@mci4me.at>
2025-11-07 09:49:48 +0100

    fix: <C5.py> main fkt added

--- src/C_Programming_paradigms/C5.py
+++ src/C_Programming_paradigms/C5.py
@@ -1,15 +1,19 @@
-square = lambda a: a ** 2 # fkt sqr for map()
-iterator = map(square, range(1, 5)) 
+def main() -> None:
+    square = lambda a: a ** 2 # fkt sqr for map()
+    iterator = map(square, range(1, 5)) 
 
-for i in iterator:
-    print(i)
+    for i in iterator:
+        print(i)
 
-print(f"{list(iterator) = }")
-iterator = map(square, range(1, 5))
-print(f"{list(iterator) = }")
+    print(f"{list(iterator) = }")
+    iterator = map(square, range(1, 5))
+    print(f"{list(iterator) = }")
 
-# map() is a 'lazy' fkt, it doesn't compute until you iterate over it.
-# the computed values are 'one-time-use'
-#   the values are computed in line 5 and printed to the console 
-#   in line 7 the computed values aren't saved in 'iterator' anymore -> list is empty
-#   in line 9 the values are computed newly 
+    # map() is a 'lazy' fkt, it doesn't compute until you iterate over it.
+    # the computed values are 'one-time-use'
+    #   the values are computed in line 5 and printed to the console 
+    #   in line 7 the computed values aren't saved in 'iterator' anymore -> list is empty
+    #   in line 9 the values are computed newly 
+    
+if __name__ == "__main__":
+    main()
