3e07a3ce77d94edea4e0a83b07c5ea13d9e73be7
TilNeubauer <nt1703@mci4me.at>
2025-11-07 09:42:20 +0100

    feat: new file <C5.py> for Exercise C.5

---
+++ src/C_Programming_paradigms/C5.py
@@ -0,0 +1,15 @@
+square = lambda a: a ** 2 # fkt sqr for map()
+iterator = map(square, range(1, 5)) 
+
+for i in iterator:
+    print(i)
+
+print(f"{list(iterator) = }")
+iterator = map(square, range(1, 5))
+print(f"{list(iterator) = }")
+
+# map() is a 'lazy' fkt, it doesn't compute until you iterate over it.
+# the computed values are 'one-time-use'
+#   the values are computed in line 5 and printed to the console 
+#   in line 7 the computed values aren't saved in 'iterator' anymore -> list is empty
+#   in line 9 the values are computed newly 
