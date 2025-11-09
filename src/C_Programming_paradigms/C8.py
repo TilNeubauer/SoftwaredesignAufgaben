cdb8755029d6fa712ecfbe3c4967708be96854de
TilNeubauer <nt1703@mci4me.at>
2025-11-09 11:36:06 +0100

    feat: new file <C8.py> and <C8module.py> for Exercise C.8

---
+++ src/C_Programming_paradigms/C8.py
@@ -0,0 +1,11 @@
+import C8module as m
+
+def main() -> None:
+    print(m.check_leap_yaer(1952))
+    
+    print(m.day_of_date(19,11,2002))
+    
+    print(f"{m.week_in_year(10,10,2024) = }")
+
+if __name__ == "__main__":
+    main()
